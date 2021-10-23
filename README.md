f7fc6fac9485a91d64f882681544b79162f67c5d
Thiago Pachêco Andrade Pereira <thiagopacheco8@gmail.com>
2021-10-23 08:38:36 -0300

    Find by id implemented

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # dio-bootcamp-gft-personapi
 Small project developed during GFT bootcamp
 
-- Create person working with service layer and DTO to model mapper.
+- Create person working with service layer and DTO to model mapper (POST /api/v1/people)
+- List all people implemented (GET /api/v1/people)
+- Find by id implemented (GET /api/v1/people/{id})
--- personapi/src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -3,12 +3,14 @@ package one.digitalinnovation.personapi.service;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,6 +21,18 @@ public class PersonService {
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream().map(personMapper::toDTO).collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Optional<Person> optionalPerson = personRepository.findById(id);
+        if(optionalPerson.isEmpty())
+            throw new PersonNotFoundException(id);
+        return personMapper.toDTO(optionalPerson.get());
+    }
+
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
         Person savedPerson = personRepository.save(personToSave);
@@ -28,8 +42,4 @@ public class PersonService {
                 .build();
     }
 
-    public List<PersonDTO> listAll() {
-        List<Person> allPeople = personRepository.findAll();
-        return allPeople.stream().map(personMapper::toDTO).collect(Collectors.toList());
-    }
 }
